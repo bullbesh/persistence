89a92bbc1de4d3a870068473b0283ee9c23fb4bf
bullbesh <babulehaha@gmail.com>
2021-04-20 19:33:49 +0300

    black

--- keyboard.py
+++ keyboard.py
@@ -1,74 +1,138 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-cherep = 'Череповец'
-moscow = 'Москва'
-peter = 'Санкт-Петербург'
-kolpino = 'Колпино'
-vorkuta = 'Воркута'
-belgorod = 'Белгород'
-yakov = 'Яковлево'
-kosto = 'Костомукша'
-milk = 'Шексна'
-work = 'Строитель'
-voron = 'Воронеж'
-sam = 'Самара'
-yarik = 'Ярославль'
-nn = 'Нижний Новгород'
-mm = 'Мурманск'
-kaz = 'Казань'
-nab_chel = 'Набережные Челны'
-bat = 'Батайск'
-pod = 'Подольск'
-mla = 'Моссан-лез-Альпий'
-volgograd = 'Волгоград'
-red_sul = 'Красный Сулин'
-stupino = 'Ступино'
-Tver = 'Тверь'
-list1 = ['Череповец', 'Москва', 'Санкт-Петербург', 'Воркута', 'Белгород', 'Яковлево', 'Костомукша', 'Шексна',
-         'Строитель', 'Ярославль', 'Воронеж', 'Самара', 'Колпино', 'Нижний Новгород', 'Мурманск', 'Казань',
-         'Набережные Челны', 'Батайск', 'Подольск', 'Моссан-лез-Альпий', 'Волгоград', 'Красный Сулин', 'Ступино', 'Тверь']
-stock = 'Узнать стоимсть акций Северстали'
-history = 'История Северстали'
-helpp = 'Помощь'
-vac = 'Вакансии'
+cherep = "Череповец"
+moscow = "Москва"
+peter = "Санкт-Петербург"
+kolpino = "Колпино"
+vorkuta = "Воркута"
+belgorod = "Белгород"
+yakov = "Яковлево"
+kosto = "Костомукша"
+milk = "Шексна"
+work = "Строитель"
+voron = "Воронеж"
+sam = "Самара"
+yarik = "Ярославль"
+nn = "Нижний Новгород"
+mm = "Мурманск"
+kaz = "Казань"
+nab_chel = "Набережные Челны"
+bat = "Батайск"
+pod = "Подольск"
+mla = "Моссан-лез-Альпий"
+volgograd = "Волгоград"
+red_sul = "Красный Сулин"
+stupino = "Ступино"
+Tver = "Тверь"
+list1 = [
+    "Череповец",
+    "Москва",
+    "Санкт-Петербург",
+    "Воркута",
+    "Белгород",
+    "Яковлево",
+    "Костомукша",
+    "Шексна",
+    "Строитель",
+    "Ярославль",
+    "Воронеж",
+    "Самара",
+    "Колпино",
+    "Нижний Новгород",
+    "Мурманск",
+    "Казань",
+    "Набережные Челны",
+    "Батайск",
+    "Подольск",
+    "Моссан-лез-Альпий",
+    "Волгоград",
+    "Красный Сулин",
+    "Ступино",
+    "Тверь",
+]
+stock = "Узнать стоимсть акций Северстали"
+history = "История Северстали"
+helpp = "Помощь"
+vac = "Вакансии"
 
-manufacture = 'Производство'
-IT_and_Digital = 'IT & Digital'
-office = 'Офис'
-Young_professionals = 'Молодым специалистам'
-back = 'Назад'
+manufacture = "Производство"
+IT_and_Digital = "IT & Digital"
+office = "Офис"
+Young_professionals = "Молодым специалистам"
+back = "Назад"
 
-more1 = 'Ещё'
-more2 = 'Больше'
+more1 = "Ещё"
+more2 = "Больше"
 
 btnStock = KeyboardButton(stock)
 btnkb = KeyboardButton(history)
 hlp = KeyboardButton(helpp)
 btnV = KeyboardButton(vac)
-markup1 = ReplyKeyboardMarkup(resize_keyboard=True).add(btnStock).add(btnkb).add(btnV).add(hlp)
-markup2 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton(manufacture)) \
-    .add(KeyboardButton(IT_and_Digital)).add(KeyboardButton(office)).add(KeyboardButton(Young_professionals)) \
+markup1 = (
+    ReplyKeyboardMarkup(resize_keyboard=True)
+    .add(btnStock)
+    .add(btnkb)
+    .add(btnV)
+    .add(hlp)
+)
+markup2 = (
+    ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    .add(KeyboardButton(manufacture))
+    .add(KeyboardButton(IT_and_Digital))
+    .add(KeyboardButton(office))
+    .add(KeyboardButton(Young_professionals))
     .add(KeyboardButton(back))
-markup3 = ReplyKeyboardMarkup(resize_keyboard=True).row(KeyboardButton(cherep), KeyboardButton(moscow)) \
-    .add(KeyboardButton(peter)).row(KeyboardButton(kolpino), KeyboardButton(vorkuta), KeyboardButton(belgorod), \
-                                    KeyboardButton(yakov)).row(KeyboardButton(kosto), KeyboardButton(milk), \
-                                                               KeyboardButton(work))
-markup4 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(KeyboardButton(kolpino),
-                                                                                KeyboardButton(moscow),
-                                                                                KeyboardButton(belgorod)) \
-    .row(KeyboardButton(peter), KeyboardButton(kosto)).row(KeyboardButton(cherep), KeyboardButton(voron),
-                                                           KeyboardButton(yarik))
-markup5 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(KeyboardButton(peter),
-                                                                                KeyboardButton(mla)) \
-    .row(KeyboardButton(moscow), KeyboardButton(cherep), KeyboardButton(sam), KeyboardButton(kolpino)) \
-    .row(KeyboardButton(yarik), KeyboardButton(kaz), KeyboardButton(bat), KeyboardButton(pod)) \
-    .row(KeyboardButton(volgograd), KeyboardButton(voron), KeyboardButton(stupino), KeyboardButton(Tver)) \
+)
+markup3 = (
+    ReplyKeyboardMarkup(resize_keyboard=True)
+    .row(KeyboardButton(cherep), KeyboardButton(moscow))
+    .add(KeyboardButton(peter))
+    .row(
+        KeyboardButton(kolpino),
+        KeyboardButton(vorkuta),
+        KeyboardButton(belgorod),
+        KeyboardButton(yakov),
+    )
+    .row(KeyboardButton(kosto), KeyboardButton(milk), KeyboardButton(work))
+)
+markup4 = (
+    ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    .row(KeyboardButton(kolpino), KeyboardButton(moscow), KeyboardButton(belgorod))
+    .row(KeyboardButton(peter), KeyboardButton(kosto))
+    .row(KeyboardButton(cherep), KeyboardButton(voron), KeyboardButton(yarik))
+)
+markup5 = (
+    ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    .row(KeyboardButton(peter), KeyboardButton(mla))
+    .row(
+        KeyboardButton(moscow),
+        KeyboardButton(cherep),
+        KeyboardButton(sam),
+        KeyboardButton(kolpino),
+    )
+    .row(
+        KeyboardButton(yarik),
+        KeyboardButton(kaz),
+        KeyboardButton(bat),
+        KeyboardButton(pod),
+    )
+    .row(
+        KeyboardButton(volgograd),
+        KeyboardButton(voron),
+        KeyboardButton(stupino),
+        KeyboardButton(Tver),
+    )
     .row(KeyboardButton(nn), KeyboardButton(red_sul))
-markup6 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton(yarik)).add(
-    KeyboardButton(cherep))
+)
+markup6 = (
+    ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    .add(KeyboardButton(yarik))
+    .add(KeyboardButton(cherep))
+)
 
-keyboards = {'Производство': markup3,
-             'IT & Digital': markup4,
-             'Офис': markup5,
-             'Молодым специалистам': markup6,
-             }
+keyboards = {
+    "Производство": markup3,
+    "IT & Digital": markup4,
+    "Офис": markup5,
+    "Молодым специалистам": markup6,
+}
--- main.py
+++ main.py
@@ -16,53 +16,72 @@ bot = Bot(token=token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+
 class vaca(StatesGroup):
     vacation = State()
     city = State()
 
-@dp.message_handler(commands=['start'], state='*')
-async def send_welcome(message: types.Message):
-    await message.answer("Здравствуйте!!!\nЯ Северствль бот\nЧтобы ознакомиться с моими функциями на клавиатуре "
-                         "нажмите на кнопку 'Помощь'", reply_markup=kb.markup1)
 
-@dp.message_handler(commands=['help'], state='*')
+@dp.message_handler(commands=["start"], state="*")
+async def send_welcome(message: types.Message):
+    await message.answer(
+        "Здравствуйте!!!\nЯ Северствль бот\nЧтобы ознакомиться с моими функциями на клавиатуре "
+        "нажмите на кнопку 'Помощь'",
+        reply_markup=kb.markup1,
+    )
+
+
+@dp.message_handler(commands=["help"], state="*")
 async def send_help(message):
     await message.answer()
 
-@dp.message_handler(TextFilter(equals=kb.stock), state='*')
+
+@dp.message_handler(TextFilter(equals=kb.stock), state="*")
 async def send_stock(message: types.Message):
     from stock import stocks
+
     await message.answer(stocks())
 
-@dp.message_handler(TextFilter(equals=kb.history), state='*')
+
+@dp.message_handler(TextFilter(equals=kb.history), state="*")
 async def send_history(message):
     from history import history_of_severstal
+
     await message.answer(history_of_severstal())
 
-@dp.message_handler(TextFilter(equals=kb.vac), state='*')
+
+@dp.message_handler(TextFilter(equals=kb.vac), state="*")
 async def send_direction(message):
     await vaca.vacation.set()
-    await message.answer('Выберите направление', reply_markup=kb.markup2)
+    await message.answer("Выберите направление", reply_markup=kb.markup2)
 
-@dp.message_handler(lambda message: message.text in ['Производство', 'IT & Digital', 'Офис', 'Молодым специалистам'], state=vaca.vacation)
+
+@dp.message_handler(
+    lambda message: message.text
+    in ["Производство", "IT & Digital", "Офис", "Молодым специалистам"],
+    state=vaca.vacation,
+)
 async def send_vacationM(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['vacation'] = message.text
+        data["vacation"] = message.text
     await vaca.next()
-    await message.answer('Выберите город', reply_markup=kb.keyboards[data['vacation']])
+    await message.answer("Выберите город", reply_markup=kb.keyboards[data["vacation"]])
+
 
 @dp.message_handler(lambda message: message.text in kb.list1, state=vaca.city)
 async def send_vactions(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['city'] = message.text
+        data["city"] = message.text
 
-    await message.answer(direction[data['vacation']][data['city']], reply_markup=kb.markup1)
-
+    await message.answer(
+        direction[data["vacation"]][data["city"]], reply_markup=kb.markup1
+    )
 
 
 @dp.message_handler()
 async def send_welcome(message):
-    await message.answer('Воспользуйтесь, пожалуйста, клавиатурой')
+    await message.answer("Воспользуйтесь, пожалуйста, клавиатурой")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
