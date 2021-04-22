99b5592c4f623af02030d3a940a9e397b74a66c2
bullbesh <babulehaha@gmail.com>
2021-04-22 18:03:49 +0300

    keyboard

--- keyboard.py
+++ keyboard.py
@@ -94,7 +94,7 @@ btnV = KeyboardButton(vac)
 markup1 = (
     ReplyKeyboardMarkup(resize_keyboard=True)
     .row(KeyboardButton(financial_summary), KeyboardButton(about_company))
-    .row(KeyboardButton(vac), KeyboardButton(hlp))
+    .row(KeyboardButton(vac), KeyboardButton(hlpp))
 )
 
 markupF = (
