c9f92d25239fbbc881106ac96a6d31e57433655a
Masynchin <masynchin@gmail.com>
2021-08-06 13:45:50 +0300

    Разбил функцию `get_content` на несколько функций

--- persistence/vacancy_parser.py
+++ persistence/vacancy_parser.py
@@ -44,48 +44,70 @@ class Vacancy:
 
 def get_content(link, vacancy_direction):
     """Получение вакансий конкретного направления по переданной ссылке."""
-    driver = webdriver.Chrome()
-    driver.get(link)
-    soup = BeautifulSoup(driver.page_source, "lxml")
-    driver.quit()
+    page_content = _get_page_content(link)
+    vacancies_page_elements = _get_vacancies_page_elements(page_content)
     vacancies = []
-    vacations = soup.find_all("a", class_="all-vacancies__table-tr")
-    for vacation in vacations:
-        vacancy_description = vacation.find(
-            "div", class_="all-vacancies__table-main-title"
-        ).get_text(strip=True)
-        vacancy_city = vacation.find(
-            "div", class_="all-vacancies__table-main-sub-title"
-        ).get_text(strip=True)
-        vacancy_information = vacation.find_all(
-            "div",
-            class_="all-vacancies__table-td-content",
+    for vacancy_page_element in vacancies_page_elements:
+        vacancy = _get_vacancy_from_page_element(
+            vacancy_page_element, vacancy_direction
         )
-        (
-            vacancy_date_publication,
-            vacancy_work_experience,
-            vacancy_employment,
-            vacancy_schedule,
-            vacancy_salary,
-        ) = (i.get_text(strip=True) for i in vacancy_information)
-        if vacancy_city not in CITIES:
+        if vacancy.city not in CITIES:
             continue
-        if vacancy_salary == "":
-            vacancy_salary = "Не указано"
-        vacancies.append(
-            Vacancy(
-                vacancy_direction=vacancy_direction,
-                vacancy_description=vacancy_description,
-                vacancy_city=vacancy_city,
-                vacancy_date_publication=vacancy_date_publication,
-                vacancy_work_experience=vacancy_work_experience,
-                vacancy_employment=vacancy_employment,
-                vacancy_schedule=vacancy_schedule,
-                vacancy_salary=vacancy_salary,
-            )
-        )
+
+        if not vacancy.salary:
+            vacancy.salary = "Не указано"
+
+        vacancies.append(vacancy)
 
     return vacancies
+
+
+def _get_page_content(url):
+    """Получение html-контента страницы по переданной ссылке."""
+    with webdriver.Chrome() as driver:
+        driver.get(url)
+        return driver.page_source
+
+
+def _get_vacancies_page_elements(page_content):
+    """Получение элементов вакансий со страницы сайта."""
+    soup = BeautifulSoup(page_content, "lxml")
+    vacancies_page_elements = soup.find_all(
+        "a", class_="all-vacancies__table-tr"
+    )
+    return vacancies_page_elements
+
+
+def _get_vacancy_from_page_element(page_element, vacancy_direction):
+    """Получение вакансии из элемента вакансии со страницы."""
+    vacancy_description = page_element.find(
+        "div", class_="all-vacancies__table-main-title"
+    ).get_text(strip=True)
+    vacancy_city = page_element.find(
+        "div", class_="all-vacancies__table-main-sub-title"
+    ).get_text(strip=True)
+    vacancy_information = page_element.find_all(
+        "div",
+        class_="all-vacancies__table-td-content",
+    )
+    (
+        vacancy_date_publication,
+        vacancy_work_experience,
+        vacancy_employment,
+        vacancy_schedule,
+        vacancy_salary,
+    ) = (i.get_text(strip=True) for i in vacancy_information)
+
+    return Vacancy(
+        vacancy_direction=vacancy_direction,
+        vacancy_description=vacancy_description,
+        vacancy_city=vacancy_city,
+        vacancy_date_publication=vacancy_date_publication,
+        vacancy_work_experience=vacancy_work_experience,
+        vacancy_employment=vacancy_employment,
+        vacancy_schedule=vacancy_schedule,
+        vacancy_salary=vacancy_salary,
+    )
 
 
 def get_vacancies():
